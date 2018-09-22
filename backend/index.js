c67a709541655ab5f94ad6fa3842b4678166cb06
Abhinav Chanda <abhinav.chanda99@gmail.com>
2018-09-22 01:20:51 -0400

    Fixed start up issues.

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const request = require('request');
-const bodyParser = require('body-parser');n
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-app.use();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 3001;
 app.listen(PORT);
+console.log("*********** Server is running on port " + PORT +" *******************");
 
 app.post('/register', (req, res) => {
 
